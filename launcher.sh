b3aa70f35584b56c09b96a10e6a3ff71eff8a634
Oliver Grawert <ogra@ubuntu.com>
2020-04-05 11:54:22 +0000

    move to dynamic UI scaling based on DPI

--- launcher.sh
+++ launcher.sh
@@ -40,4 +40,29 @@ if [ "$(ls -A $NV_PATH)" ]; then
     echo "" >>"$LOGFILE"
 fi
 
+# scale according to dpi
+DPI="$(xrdb -query 2>/dev/null| grep dpi | sed 's/^.*\t//')"
+if [ -n "$DPI" ]; then
+  if [ "$DPI" -le 96 ]; then
+          SCALE=""
+  elif [ "$DPI" -le 120 ]; then
+          SCALE=1.25
+  elif [ "$DPI" -le 144 ]; then
+          SCALE=1.5
+  elif [ "$DPI" -le 192 ]; then
+          SCALE=2
+  elif [ "$DPI" -le 240 ]; then
+          SCALE=2.5
+  elif [ "$DPI" -le 288 ]; then
+          SCALE=3
+  elif [ "$DPI" -le 384 ]; then
+          SCALE=4
+  fi
+fi
+
+if [ -n "$SCALE" ]; then
+  echo "$DPI dpi, using scale factor: $SCALE" >>"$LOGFILE" 2>&1
+  export QT_SCALE_FACTOR="$SCALE"
+fi
+
 exec $SNAP/zoom/ZoomLauncher "$@" >> "$LOGFILE" 2>&1
